9030563d9acb944cbc4d5825d1d9fc8e14bc36be
Franco Gaytan <gaytanfranco@gmail.com>
2022-12-12 16:15:06 -0300

    first page renderized

---
+++ eat-asado/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login  from './Pages/Login/Login';
+
+export default function EatAsado(): JSX.Element {
+	return (
+	<Login />
+  );
+}
---
+++ eat-asado/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import EatAsado from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <EatAsado />
+  </React.StrictMode>
+);
+
+reportWebVitals();
+
