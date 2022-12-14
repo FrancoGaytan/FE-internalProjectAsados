ed801f93701daf8272b30bc905b1af7a6952ea2d
Franco Gaytan <gaytanfranco@gmail.com>
2022-12-14 11:51:22 -0300

    minor changes

--- eat-asado/src/App.tsx
+++ eat-asado/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './Pages/Login/login';
 import routes, { IRoute } from './routes';
 import { GlobalProvider } from './stores/GlobalContext';
 import { LocalizationProvider } from './stores/LocalizationContext';
