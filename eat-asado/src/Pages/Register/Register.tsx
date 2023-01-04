78458f25688190dc809db88feee193d27a32bfa6
Franco Gaytan <gaytanfranco@gmail.com>
2023-01-04 17:12:27 -0300

    new styles at register page

--- eat-asado/src/Pages/Register/Register.tsx
+++ eat-asado/src/Pages/Register/Register.tsx
@@ -7,16 +7,32 @@ const Register = () => {
 	const translation = useTranslation('login');
 
 	return (
+		//todo: meter todos los inputs y label adentro de un contenedor para manipular mejor el ancho y luego aplicar grid en desk
 		<FormLayout>
-			<p className={styles.title}>REGISTER</p>
+			<div className={styles.closeBtn}></div>
+			<label className={styles.title}>REGISTER</label>
+			<label htmlFor="nombre" className={styles.registerLabel}>
+				Nombre
+			</label>
+			<input id="nombre" placeholder="Nombre" type="text" />
+			<label htmlFor="apellido" className={styles.registerLabel}>
+				Apellido
+			</label>
+			<input id="apellido" placeholder="Apellido" type="text" />
 			<label htmlFor="email" className={styles.registerLabel}>
-				Email Endava
+				Email
 			</label>
-			<input id="email" placeholder="EMAIL" type="text" />
-			<label htmlFor="password" className={styles.registerLabel}>
-				Clave
+			<input id="email" placeholder="user@endava.com" type="text" />
+			<span className={styles.inputDescription}>Utilizar por favor el email de endava</span>
+			<label htmlFor="contraseña" className={styles.registerLabel}>
+				Contraseña
 			</label>
-			<input id="password" placeholder="CONTRASEÑA" type="password" />
+			<input id="contraseña" placeholder="" type="password" />
+			<span className={styles.inputDescription}>La clave debe ser alfanumerica y contener un minimo de 8 caracteres</span>
+			<label htmlFor="confirmarContraseña" className={styles.registerLabel}>
+				Comfirmar Contraseña
+			</label>
+			<input id="confirmarContraseña" placeholder="" type="password" />
 			<Button kind="primary" size="large">
 				REGISTER
 			</Button>
