02d2a237b48334bb3b7c9b6944cb32e16c8c5511
Franco Gaytan <gaytanfranco@gmail.com>
2023-01-17 17:24:18 -0300

    register columns added

--- eat-asado/src/Pages/Register/Register.tsx
+++ eat-asado/src/Pages/Register/Register.tsx
@@ -11,28 +11,54 @@ const Register = () => {
 		<FormLayout>
 			<div className={styles.closeBtn}></div>
 			<label className={styles.title}>REGISTER</label>
-			<label htmlFor="nombre" className={styles.registerLabel}>
-				Nombre
-			</label>
-			<input id="nombre" placeholder="Nombre" type="text" />
-			<label htmlFor="apellido" className={styles.registerLabel}>
-				Apellido
-			</label>
-			<input id="apellido" placeholder="Apellido" type="text" />
-			<label htmlFor="email" className={styles.registerLabel}>
-				Email
-			</label>
-			<input id="email" placeholder="user@endava.com" type="text" />
-			<span className={styles.inputDescription}>Utilizar por favor el email de endava</span>
-			<label htmlFor="contraseña" className={styles.registerLabel}>
-				Contraseña
-			</label>
-			<input id="contraseña" placeholder="" type="password" />
-			<span className={styles.inputDescription}>La clave debe ser alfanumerica y contener un minimo de 8 caracteres</span>
-			<label htmlFor="confirmarContraseña" className={styles.registerLabel}>
-				Comfirmar Contraseña
-			</label>
-			<input id="confirmarContraseña" placeholder="" type="password" />
+			<div className={styles.inputSection}>
+				<section className={styles.firstColumn}>
+					<label htmlFor="nombre" className={styles.registerLabel}>
+						Nombre
+					</label>
+					<input id="nombre" placeholder="Nombre" type="text" />
+					<label htmlFor="apellido" className={styles.registerLabel}>
+						Apellido
+					</label>
+					<input id="apellido" placeholder="Apellido" type="text" />
+					<label htmlFor="email" className={styles.registerLabel}>
+						Email
+					</label>
+					<input id="email" placeholder="user@endava.com" type="text" />
+					<span className={styles.inputDescription}>Utilizar por favor el email de endava</span>
+				</section>
+				<section className={styles.secondColumn}>
+					<label htmlFor="contraseña" className={styles.registerLabel}>
+						Contraseña
+					</label>
+					<input id="contraseña" placeholder="" type="password" />
+					<span className={styles.inputDescription}>La clave debe ser alfanumerica y contener un minimo de 8 caracteres</span>
+					<label htmlFor="confirmarContraseña" className={styles.registerLabel}>
+						Comfirmar Contraseña
+					</label>
+					<input id="confirmarContraseña" placeholder="" type="password" />
+					<section className={styles.checkboxesContainer}>
+						<div className={styles.internalTitle}>
+							<label className={styles.title}>CONTANOS DE VOS</label>
+							<span className={styles.extraDescription}>(opcional)</span>
+						</div>
+						<label className={styles.registerLabel}>
+							<input id="isVegan" type="checkbox" className={styles.checkbox} />
+							Sos Vegano?
+						</label>
+
+						<label className={styles.registerLabel}>
+							<input id="isVegetarian" type="checkbox" className={styles.checkbox} />
+							Sos Vegetariano?
+						</label>
+
+						<label className={styles.registerLabel}>
+							<input id="isHypertensive" type="checkbox" className={styles.checkbox} />
+							Sos Hipertenso?
+						</label>
+					</section>
+				</section>
+			</div>
 			<Button kind="primary" size="large">
 				REGISTER
 			</Button>
