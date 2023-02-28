43d4ac0032f8ac8b0147868d1fc067f370903653
Martin Lazarte <martin.lazarte@endava.com>
2023-02-28 16:46:09 -0300

    Added drag and drop functionality

---
+++ eat-asado/src/Components/micro/DragAndDrop/DragAndDrop.tsx
@@ -0,0 +1,50 @@
+import { Dispatch, ReactElement, SetStateAction, useEffect, useRef } from "react";
+import { UserProfileInterface } from "../../../Pages/UserProfile/UserProfile";
+
+interface IDragAndDrop {
+    children: ReactElement,
+    setState: Function
+}
+
+const DragAndDrop = (props: IDragAndDrop) => {
+    const dropRef = useRef<HTMLDivElement>(null);
+
+    const onUpload = (files: FileList) => {
+        props.setState(files[0]);
+	}
+    
+    useEffect(() => {
+		dropRef.current?.addEventListener('dragover', handleDragOver);
+		dropRef.current?.addEventListener('drop', handleDrop);
+
+		return () => {
+			dropRef.current?.removeEventListener('dragover', handleDragOver);
+			dropRef.current?.removeEventListener('drop', handleDrop);
+		};
+	}, []);
+
+	const handleDragOver = (e: DragEvent) => {
+		e.preventDefault();
+		e.stopPropagation();
+	};
+
+	const handleDrop = (e: DragEvent) => {
+		e.preventDefault();
+  		e.stopPropagation();
+
+        const dataTransfer = e.dataTransfer as DataTransfer
+
+		const files = dataTransfer.files;
+		if(files && files.length) {
+			onUpload(files);
+		}
+	}
+
+    return (
+        <div ref={dropRef}>
+            {props.children}
+        </div>
+    );
+}
+
+export default DragAndDrop;
--- eat-asado/src/Pages/UserProfile/UserProfile.tsx
+++ eat-asado/src/Pages/UserProfile/UserProfile.tsx
@@ -1,11 +1,12 @@
 import styles from './styles.module.scss';
 import Button from '../../Components/micro/Button/Button';
 import { useTranslation } from '../../stores/LocalizationContext';
-import { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import DragAndDrop from '../../Components/micro/DragAndDrop/DragAndDrop';
 
-interface UserProfileInterface {
-	userImage?: any;
-	userCbu?: number;
+export interface UserProfileInterface {
+	userImage?: File;
+	userCbu?: string;
 	userAlias?: string;
 	userVegan?: boolean;
 	userVegetarian?: boolean;
@@ -15,21 +16,42 @@ interface UserProfileInterface {
 
 const UserProfile = () => {
 	const lang = useTranslation('userProfile');
+	let emptyFile = undefined as unknown as File;
+
 	const initialUser = {
-		userImage: '',
-		userCbu: undefined,
+		userImage: emptyFile,
+		userCbu: '',
 		userAlias: '',
 		userVegan: false,
 		userVegetarian: false,
 		userHypertensive: false,
 		userCeliac: false
 	};
-
+	
+	const inputRef = useRef<HTMLInputElement>(null);
 	const [user, setUser] = useState<UserProfileInterface>(initialUser);
-
-	const handleSubmit = () => {
+	
+	const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault()
 		console.log(user);
 	};
+
+	const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+		e.preventDefault();
+		e.stopPropagation();
+		const target = e.target;
+
+		//Prevents deleting current image when user press Cancel in the dialog window
+		if(target.files?.length && target.files.length > 0) {
+			setUser({...user, userImage: target.files[0]})
+		}
+	}
+
+	const setProfileImage = (file: File) => {
+		console.log(user);
+		setUser({...user, userImage: file})
+		console.log(user);
+	}
 
 	return (
 		<div className={styles.userProfileContainer}>
@@ -38,8 +60,14 @@ const UserProfile = () => {
 				<div className={styles.firstColumnProfile}>
 					<h3>{lang.personalData}</h3>
 					<div className={styles.pictureRow}>
-						<div className={styles.userPicture}></div>
-						<p>{lang.editImg}</p>
+						<DragAndDrop setState={setProfileImage}>
+							{ user.userImage !== undefined ? 
+								<img src={URL.createObjectURL(user.userImage)} className={styles.userPicture} alt='selected'/> :
+								<img src='/assets/pictures/profile.png' className={styles.userPicture} alt='placeholder'/>
+							}
+						</DragAndDrop>
+						<p onClick={() => inputRef.current?.click()} style={{cursor: 'pointer'}}>{lang.editImg}</p>
+						<input type="file" style={{display: 'none'}} onChange={handleFile} ref={inputRef} />
 					</div>
 					<label htmlFor="cbu" className={styles.cbuLabel}>
 						{lang.cbu}
@@ -51,7 +79,7 @@ const UserProfile = () => {
 						type="text"
 						value={user.userCbu}
 						onChange={e => {
-							setUser({ ...user, userCbu: Number(e.target.value) });
+							setUser({ ...user, userCbu: e.target.value});
 						}}
 					/>
 					<label htmlFor="alias" className={styles.cbuLabel}>
