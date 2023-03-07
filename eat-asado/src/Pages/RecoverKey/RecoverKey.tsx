ceb4bc05f1edc4484a3fa9043375e527ee345cac
Martin Lazarte <martin.lazarte@endava.com>
2023-03-07 13:03:40 -0300

    Added functionality to RecoverKey page

--- eat-asado/src/Pages/RecoverKey/RecoverKey.tsx
+++ eat-asado/src/Pages/RecoverKey/RecoverKey.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.scss';
 import Button from '../../Components/micro/Button/Button';
 import PrivateFormLayout from '../../Components/macro/layout/PrivateFormLayout';
@@ -6,6 +6,15 @@ import { useTranslation } from '../../stores/LocalizationContext';
 
 const RecoverKey = () => {
 	const lang = useTranslation('recoverKey');
+	const [userEmail, setUserEmail] = useState('');
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUserEmail(e.target.value);
+	}
+
+	const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+		console.log(userEmail);
+	}
 
 	return (
 		<div>
@@ -16,9 +25,20 @@ const RecoverKey = () => {
 					<label htmlFor="Email" className={styles.emailLabel}>
 						{lang.email}
 					</label>
-					<input className={styles.input} id="email" placeholder={lang.email} type="text" />
-					<Button kind="primary" size="large" id="registerBtn" style={{ marginBottom: 30 }}>
-						{lang.sendEmail}
+					<input 
+						className={styles.input} 
+						id="email" 
+						placeholder={lang.email} 
+						type="text" 
+						value={userEmail} 
+						onChange={handleChange}/>
+					<Button 
+						kind="primary" 
+						size="large" 
+						id="registerBtn" 
+						style={{ marginBottom: 30 }} 
+						onClick={handleSubmit}>
+							{lang.sendEmail}
 					</Button>
 				</div>
 			</PrivateFormLayout>
