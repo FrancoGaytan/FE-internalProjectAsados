c8f2b09f3a45254f082a526c7687a2222d1750e4
Franco Gaytan <gaytanfranco@gmail.com>
2023-03-21 12:32:35 -0300

    adding describe to test.js file prev expect

--- eat-asado/src/Components/micro/Button/test.test.js
+++ eat-asado/src/Components/micro/Button/test.test.js
@@ -2,14 +2,17 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Button from './Button';
 
-test('renderizado del componente', () => {
-	render(<Button />);
-	const btn = screen.getByRole('button');
-	expect(btn).toBeInTheDocument();
-});
+describe('UNIT - Node', () => {
+	test('el componente boton, debe ser renderizado', () => {
+		expect(screen.getBy('button')).not.toBeInTheDocument(); //initial state
+		render(<Button />); //change
+		const btn = screen.getByRole('button');
+		expect(btn).toBeInTheDocument(); //verify change
+	});
 
-test('correcto renderizado del texto del boton', () => {
-	render(<Button>Login</Button>);
-	const btnText = screen.getByText('Login');
-	expect(btnText).toBeInTheDocument();
+	test('correcto renderizado del texto del boton', () => {
+		render(<Button>Login</Button>);
+		const btnText = screen.getByText('Login');
+		expect(btnText).toBeInTheDocument();
+	});
 });
