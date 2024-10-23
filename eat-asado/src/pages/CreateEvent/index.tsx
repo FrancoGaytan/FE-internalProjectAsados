de32496c657520dbb58c901156ad5d506ccfa15e
Franco Gaytan <gaytanfranco@gmail.com>
2024-10-23 09:57:08 -0300

    new navigate change

--- eat-asado/src/pages/CreateEvent/index.tsx
+++ eat-asado/src/pages/CreateEvent/index.tsx
@@ -76,15 +76,14 @@ export function CreateEvent(): JSX.Element {
 			editEvent(eventIdParam, event)
 				.then(res => {
 					setAlert(`${lang.eventUpdateConfirmation}!`, AlertTypes.SUCCESS);
-					handleGoBack();
 				})
 				.catch(e => setAlert(`${e}`, AlertTypes.ERROR))
 				.finally(() => setIsLoading(false));
-			navigate(`/event/${eventIdParam}`);
+			setTimeout(() => {
+				navigate(`/event/${eventIdParam}`);
+			}, 1000);
 		}
 	}
-
-	console.log(eventIdParam);
 
 	useEffect(() => {
 		const abortController = new AbortController();
