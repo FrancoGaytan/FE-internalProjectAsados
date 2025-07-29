e2c4f4e12ca0a878fff861c5e9e42f1f4f4919f3
FrancoGaytanEndava <franco.gaytan@endava.com>
2025-07-29 15:32:18 -0300

    text change

--- eat-asado/src/localization/es-ar/recoverKey.ts
+++ eat-asado/src/localization/es-ar/recoverKey.ts
@@ -2,6 +2,7 @@ export const recoverKey = Object.seal({
 	newPassword: 'Nueva Contraseña',
 	email: 'Email',
 	sendEmail: 'ENVIAR EMAIL',
-	changeDescription: 'Te enviaremos un código de verificación a tu casilla de email y con ella serás capaz de establecer una nueva contraseña.',
+	changeDescription:
+		'Te enviaremos un código de verificación a tu casilla de email y con ella serás capaz de establecer una nueva contraseña. Por favor, ingresa el email que usaste para registrarte.',
 	emailSentConfirmation: 'Correo de recuperación enviado, revisá tu bandeja de entrada.'
 });
